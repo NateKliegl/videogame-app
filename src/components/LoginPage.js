f029746b9c89a99e235404c153ecc6c8aec73659
Nate Kliegl <nkliegl2@gmail.com>
2021-10-06 14:21:37 -0700

    set up ProtectedRoute and fix LoginPage error

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { HeroContext } from "../shared/HeroContext";
 
-function LoginPage() {
-  return <div></div>;
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const { setUser } = useContext(HeroContext);
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="Username">Username</label>
+        <input
+          id="username"
+          value={username}
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        {error &&
+          username.length < 3 &&
+          "Username must be more than 3 characters"}
+      </div>
+      <div>
+        <label htmlFor="Password">Password</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        {error &&
+          password.length < 3 &&
+          "Password must be more than 3 characters"}
+      </div>
+      <button
+        onClick={() => {
+          if (username < 3 || password < 3) {
+            setError(true);
+            return;
+          }
+          setUser(username);
+        }}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
-
-export default LoginPage;
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
+import { Redirect, Route } from "react-router-dom";
+import { HeroContext } from "../shared/HeroContext";
 
-function ProtectedRoute() {
-  return <div></div>;
+function ProtectedRoute({ children, path, armor }) {
+  const { user } = useContext(HeroContext);
+
+  const redirectTo = useMemo(() => (armor ? "./login" : "./search"), [armor]);
+
+  if ((user && armor) || (!user && !armor)) {
+    return <Route path={path}>{children}</Route>;
+  } else {
+    return (
+      <Route to={path}>
+        <Redirect to={redirectTo}></Redirect>
+      </Route>
+    );
+  }
 }
 
 export default ProtectedRoute;
