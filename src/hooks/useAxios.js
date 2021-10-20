e777ee1f5a3a708c9b8798f668572583180ebe7e
Nate Kliegl <nkliegl2@gmail.com>
2021-10-20 11:37:33 -0500

    changes hook to axios

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -7,50 +7,27 @@ function useAxios(url, method, body = null) {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const url = `https://superheroapi.com/api.php/4283609898424396/search/${search}/image/powerstats/biography/appearance`;
-
     async function init() {
-      if (search.length < 3) return;
+      if (!url || (method === "post" && !body)) {
+        return;
+      }
 
       setError(null);
       setLoading(true);
-      setData([]);
+      setJson(null);
       try {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(json);
-
-        setData(() =>
-          json.results.map((hero) => ({
-            name: hero.name,
-            id: hero.id,
-            url: hero.image.url,
-            strength: hero.powerstats.strength,
-            speed: hero.powerstats.speed,
-            power: hero.powerstats.power,
-            intelligence: hero.powerstats.intelligence,
-            publisher: hero.biography.publisher,
-            fullName: hero.biography["full-name"],
-            firstShow: hero.biography["first-appearance"],
-            birth: hero.biography["place-of-birth"],
-            alignment: hero.biography.alignment,
-            gender: hero.appearance.gender,
-            race: hero.appearance.race,
-            height: hero.appearance.height,
-            weight: hero.appearance.weight,
-          }))
-        );
+        const response = await axios[method](url, body);
+        setJson(response.data);
       } catch (e) {
-        console.log(e);
         setError(e);
       } finally {
         setLoading(false);
       }
     }
     init();
-  }, [search]);
+  }, [url, body, method]);
 
-  return { data, loading, error };
+  return { json, loading, error };
 }
 
 export default useAxios;
